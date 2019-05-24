5803c8f3dbcaebba84c32f0160d84018ca395c76
samueldcabral <samueldcabral@gmail.com>
2019-05-24 02:34:25 -0300

    Alumni page created. with a form to register new alumnums and a table showing all registry also a page refresh once you persist it to the db.

--- src/alumni.php
+++ src/alumni.php
@@ -1,16 +1,54 @@
 <?php
-
-  // $host = 'mysql';
-  // $user = 'samuel';
-  // $password = 'samuel123';
-  // $dbname = 'db_php_web_exam';
+  $host = 'mysql';
+  $user = 'samuel';
+  $password = 'samuel123';
+  $dbname = 'db_php_web_exam';
 
   // setup mysql config for PDO
-  // $conf = 'mysql:host=' . $host . ';dbname=' . $dbname;
-  // $conn = new PDO($conf, $user, $password);
+  $conf = 'mysql:host=' . $host . ';dbname=' . $dbname;
+  $conn = new PDO($conf, $user, $password);
 
-  // INSERT EXAMPLES
-  
+  // SELECT EXAMPLES
+  try {
+    $sql = 'SELECT * FROM alumni';
+    $stm = $conn->prepare($sql);
+    $stm->execute();
+    $result = $stm->fetchAll();
+
+    // if(password_verify($_POST["password"], $result[0]["password"])){
+    //   usleep(300);
+    // // header('Location: http://localhost:8080/');
+    //   header('Location: alumni.php');
+    // }else {
+    //   echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    //         <strong>Ooops, wrong password!</strong> please try again.
+    //         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    //         <span aria-hidden="true">&times;</span>
+    //         </button>
+    //         </div>';
+    // }
+
+  } catch(PDOException $e) {
+    echo $e->message();
+  }
+
+  if(isset($_POST["name"])){
+    try {
+      $sql = 'INSERT INTO alumni(idalumni, name, email, linkedin, course, campus) VALUES(:idalumni, :user, :email, :linkedin, :course, :campus)';
+      $stmt = $conn->prepare($sql);
+      $stmt->execute(['idalumni' => date("y-m-d m:i:s"), 'user' => $_POST["name"], 'email' => $_POST["name"], 'linkedin' => $_POST["linkedin"], 'course' => $_POST["course"], 'campus' => $_POST["campus"]]) ;
+      
+      echo '<div class="alert alert-success" role="alert">
+                <strong>Well done!</strong> Great success!
+            </div>';
+
+      // usleep(300);\
+      // header("Refresh:0");
+
+    } catch(PDOException $e) {
+      echo 'error';
+    }
+  }
   // $stm = $conn->query("select * from user;");
   // var_dump($stm->fetchAll());
   // insert example
@@ -50,53 +88,65 @@
   <body>
     <header class="ml-4 text-center mt-4">
       <h2 class="text-primary">Check out our Alumni!</h2>
+      <a class="btn btn-outline-primary" href="/" formaction="index.php">Go back</a>
     </header>
     
     <br>
 
     <section class="container">
-      <table class="table">
+
+      <form class="form-inline" method="post" action="/alumni.php">
+        <label class="sr-only" for="inlineFormInputName2">Name</label>
+        <input type="text" class="form-control" name="name" id="inlineFormInputName2" placeholder="Name" required>
+
+        <label class="sr-only" for="inlineFormInputGroupUsername2">Email</label>
+        <input type="email" class="form-control ml-2" name="email" id="inlineFormInputGroupUsername2" placeholder="Email" required>
+  
+        <label class="sr-only" for="inlineFormInputGroupUsername2">Linkedin</label>
+        <input type="text" class="form-control ml-2" name="linkedin" id="inlineFormInputGroupUsername2" placeholder="Linkedin" required>
+
+        <select class="form-control ml-2" name="course">
+          <option>cstsi</option>
+        </select>
+
+        <select class="form-control ml-2" name="campus">
+          <option>ifpb-jp</option>
+        </select>
+
+        <button type="submit" class="btn btn-primary ml-2 align-items-centfer">Save</button>
+      </form>
+
+      <br>
+
+      <table class="table table-hover">
         <thead class="thead-light">
           <tr>
             <th>#</th>
             <th>Full Name</th>
             <th>Email</th>
+            <th>Linkedin</th>
             <th>Course</th>
             <th>Campus</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Andreza de Sousa Vieira</td>
-            <td>andreza_sv@yahoo.com.br</td>
-            <td>cstsi</td>
-            <td>ifpb-jp</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Gustavo Cavalcanti</td>
-            <td>Gustavo@Gustavo.com</td>
-            <td>cstsi</td>
-            <td>ifpb-jp</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Jonathas Santos de Lacerda</td>
-            <td>Jonathas@jonathas.com</td>
-            <td>cstsi</td>
-            <td>ifpb-jp</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>Luiz Carlos Rodrigues Chaves</td>
-            <td>lucachaves@gmail.com</td>
-            <td>cstsi</td>
-            <td>ifpb-jp</td>
-          </tr>
+          <?php 
+            $count = 1;
+            foreach($result as $user) {
+              echo '<tr>';
+              echo '<th scope="row">' . $count . '</th>';
+              echo '<td>' . $user["name"] . '</td>';
+              echo '<td>' . $user["email"] . '</td>';
+              echo '<td><a class="text-dark" href="' . $user["linkedin"] . '">Click Me!</a></td>';
+              echo '<td>' . $user["course"] . '</td>';
+              echo '<td>' . $user["campus"] . '</td>';
+              echo '</a></tr>';
+              $count++;
+            }
+          ?>
         </tbody>
       </table>
-      <a class="btn btn-primary" href="/" formaction="index.php">Go back</a>
+
     </section>
   </body>
   </html>
